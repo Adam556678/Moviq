9f50b77e30c586e3ef736ad066e7a9641ced2bf6
Adam556678 <mahmudramadan75@yahoo.com>
2026-03-09 17:50:44 +0200

    Implement thetaer event publisher

--- TheaterService/Services/AsyncDataService/EventBusPublisher.cs
+++ TheaterService/Services/AsyncDataService/EventBusPublisher.cs
@@ -1,12 +1,73 @@
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
 using TheaterService.Services.Events;
 
 namespace TheaterService.Services.AsyncDataService
 {
     public class EventBusPublisher : IEventBusPublisher
     {
-        public Task PublishShowtimePricing(ShowtimePricingPublishedEvent evnt)
+
+        private readonly IConfiguration _configuration;
+        private readonly ConnectionFactory _factory;
+
+        private IConnection? _connection;
+        private IChannel? _channel;
+        public EventBusPublisher(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+            _factory = new ConnectionFactory()
+            {
+                HostName = configuration["RabbitMQHost"] ?? "localhost",
+                Port = int.Parse(configuration["RabbitMQPort"] ?? "5672")
+            };
+        }
+
+        public async Task PublishShowtimePricing(ShowtimePricingPublishedEvent evnt)
+        {
+            var message = JsonSerializer.Serialize(evnt);
+
+            await SendMessage(
+                exchange: "theater.events",
+                routingKey: "showtime.created",
+                message
+            );
+        }
+
+         private async Task EnsureConnection(string exchange)
+        {
+            if (_channel != null && _channel.IsOpen)
+                return;
+
+            Console.WriteLine("--> Attempting to connect to RabbitMQ...");
+
+            _connection = await _factory.CreateConnectionAsync();
+            _channel = await _connection.CreateChannelAsync();
+            await _channel.ExchangeDeclareAsync(
+                exchange, 
+                ExchangeType.Direct, 
+                durable: true
+            );
+        }
+
+        private async Task SendMessage(string exchange, string routingKey, string message)
+        {
+            await EnsureConnection(exchange);
+
+            var body = Encoding.UTF8.GetBytes(message);
+            await _channel!.BasicPublishAsync(exchange, routingKey, body);
+
+            Console.WriteLine($"Message sent: {message}");
+        }
+
+        public async Task Dispose()
+        {
+            Console.WriteLine("Message Bus Disposed");
+            if (_channel != null && _connection != null && _channel.IsOpen)
+            {
+                await _channel.CloseAsync();
+                await _connection.CloseAsync();
+            }
         }
     }
 }
